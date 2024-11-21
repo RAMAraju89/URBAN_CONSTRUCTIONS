067dce984911e4f8f2956612f0c0b8812dc43fdc
RAMAraju89 <85574424+RAMAraju89@users.noreply.github.com>
2024-11-21 22:25:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import pickle
 import matplotlib
-import matplotlib.use("Agg")  # Set backend before importing pyplot or Streamlit
 import matplotlib.pyplot as plt
 
 import streamlit as st
