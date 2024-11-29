2cebbbb1f8040a533665a428ea21120eb8ef53f9
RAMAraju89 <85574424+RAMAraju89@users.noreply.github.com>
2024-11-29 15:32:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 from sklearn.model_selection import train_test_split
